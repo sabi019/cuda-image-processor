7569ae7124e94e18a740929f84a62c48d810ae8a
Sabina <aitakhmetovasabina@gmail.com>
2026-07-30 21:59:45 +0500

    Create main.cu

---
+++ src/main.cu
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit for CUDA Capstone Project"
+git branch -M main
+git remote add origin https://github.com/твоё_имя/cuda-image-processor.git
+git push -u origin main
